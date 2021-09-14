c0466ebd08e846141b48ca24c7c0f7765a761e83
Hygor Silva <77420686+hygor-silva@users.noreply.github.com>
2021-09-14 18:36:28 -0300

    Criando projeto maven
    Projeto criado no site start.spring.io , importado para o eclipse como projeto maven e início da configuração do banco de dados

---
+++ StudentManagement/src/test/java/br/com/myprojects/StudentManagement/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.myprojects.StudentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
