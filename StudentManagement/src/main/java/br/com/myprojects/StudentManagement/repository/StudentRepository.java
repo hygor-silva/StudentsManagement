657b73c4c742f654b7522136f570baabd2c80447
Hygor Silva <77420686+hygor-silva@users.noreply.github.com>
2021-09-15 11:22:00 -0300

    Correção de bug
    Correção do erro que impedia o thymeleaf de ler os dados do banco

--- StudentManagement/src/main/java/br/com/myprojects/StudentManagement/repository/StudentRepository.java
+++ StudentManagement/src/main/java/br/com/myprojects/StudentManagement/repository/StudentRepository.java
@@ -1,10 +1,11 @@
 package br.com.myprojects.StudentManagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.myprojects.StudentManagement.entity.Student;
 
-
+@Repository
 public interface StudentRepository extends JpaRepository <Student,Long>{
 	
 	
